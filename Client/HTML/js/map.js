96b9280c1b4d7490bad5f8db2cf4b6d05d5d45b3
Guilhem Albert <guilhem.albert@insa-lyon.fr>
2014-03-13 20:08:45 +0100

    Vérification du lieu pour le checkin

--- Client/HTML/js/map.js
+++ Client/HTML/js/map.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+
 google.maps.event.addDomListener(window, 'load', initialize);
 	
 function initialize() {
@@ -21,7 +22,7 @@ function initialize() {
 	function loadmap(position) {
 	
 		var mapOptions = {
-    	zoom: 18,
+    	zoom: 17,
     	center: new google.maps.LatLng(position.currentlat, position.currentlong),
 		styles : myStyles,
 		streetViewControl: false
@@ -37,7 +38,7 @@ function initialize() {
       		fillOpacity: 0.35,
       		map: map,
       		center: mapOptions.center,
-      		radius: 35,
+      		radius: 30,
    	 	};
 	
 		displaycurrentposition = new google.maps.Circle(position_options);
@@ -69,18 +70,73 @@ function initialize() {
   
    		var BMC = new google.maps.Polygon({
     		paths: BMCCoords,
-    		strokeColor: '#FF0000',
+    		strokeColor: '#00FFFF',
     		strokeOpacity: 0.8,
     		strokeWeight: 2,
-    		fillColor: '#FF0000',
-    		fillOpacity: 0.7
+    		fillColor: '#00FFFF',
+    		fillOpacity: 0.35
   		});
 
-  		GI.setMap(map);
-  		BMC.setMap(map);
-  
-  		google.maps.event.addListener(GI,'click', function() {
-	  	window.location.href="GI.html";
+		
+		var Entreprise = new Array();
+		Entreprise[0] = {Objet :GI,
+						 Chemin : "GI.html",
+						 content : [ "<b>GI</b><br/>","<button id='checkin' disabled>Check-in</button><br/>","<button id='info'>Informations</button>"].join("")
+						 };
+		Entreprise[1] = {Objet :BMC,
+						 Chemin : "BMC.html",
+						  content : [ "<b>BMC</b><br/>","<button id='checkin' disabled>Check-in</button><br/>","<button id='info'>Informations</button>"].join("")
+						 };
+		
+		//On affiche chaque entreprise sur la carte
+		for (var i=0;i<Entreprise.length;i++) {
+  			Entreprise[i].Objet.setMap(map);
+		};
+	
+		function islocationonedge (entreprise) {
+		var precision_degre = position_options.radius/111000;
+	 	if (google.maps.geometry.poly.isLocationOnEdge(mapOptions.center, entreprise,precision_degre)) {
+    	return true;
+  		}
+		else { return false};
+		}
+		
+		
+
+		var infowindow = new google.maps.InfoWindow();
+		
+
+		function show_myInfowindow(entreprise,position) {
+			infowindow.close();
+			var vertices = entreprise.Objet.getPath();
+
+			// Iterate over the vertices.
+			for (var i =0; i < vertices.length; i++) {
+				var xy = vertices.getAt(i);
+			}
+
+			// Replace our Info Window's position
+			infowindow.setContent(entreprise.content);
+			infowindow.setPosition(position);
+			infowindow.open(map);
+			if (islocationonedge(entreprise.Objet)) {
+				document.getElementById("checkin").disabled = false; 
+			}
+		} 
+		
+			
+		
+			
+		google.maps.event.addListener(Entreprise[0].Objet,'click', function(event) {
+			show_myInfowindow(Entreprise[0],event.latLng);	
+			google.maps.event.addDomListener(document.getElementById('info'), 'click', function(){window.location.href= Entreprise[0].Chemin})
+			google.maps.event.addDomListener(document.getElementById('checkin'), 'click', function(){alert("Check-in réalisé́!")})	
+  		})
+		
+		google.maps.event.addListener(Entreprise[1].Objet,'click', function(event) {
+			show_myInfowindow(Entreprise[1],event.latLng);
+			google.maps.event.addDomListener(document.getElementById('info'), 'click', function(){window.location.href= Entreprise[1].Chemin})
+			google.maps.event.addDomListener(document.getElementById('checkin'), 'click', function(){alert("Check-in réalisé!")})	
   		})
 	};
 
@@ -88,10 +144,10 @@ function initialize() {
   
   	//géolocalisation : off
     function onError(error) {
-		alert(' activer geolocalisation');
+		//alert(' activer geolocalisation');
 		joueur_position.currentlat = 45.78272115660313;
 		joueur_position.currentlong = 4.872586727142334;
-		loadmap(joueur_position);
+		loadmap(joueur_position);		
  	} 
   
 
