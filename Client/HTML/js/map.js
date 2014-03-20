608d82733121dddd3324b9e76ec0bba3d0f98917
Guilhem Albert <guilhem.albert@insa-lyon.fr>
2014-03-20 16:32:27 +0100

    a

--- Client/HTML/js/map.js
+++ Client/HTML/js/map.js
@@ -1037,33 +1037,12 @@ function initialize() {
 		
 		
 		function openinfo(entreprise) {
-			if(!$("div.modal").is(':visible')){
-       	 		$("div.modal").show(100);
-        		$("div.modalContent").delay(150).show(400);
-				$("div.modalContent").load(entreprise.Chemin);
-				infowindow.close(map);
-    		}
+			$( "#popupcontent" ).load(entreprise.Chemin);
+			$( "#popupmodal" ).popup( "open" );
+			infowindow.close(map);
 		}
   	
-		$("div.modal").click( function () {
-   			// Close the modal
-    		if($("div.modal").is(':visible')) {
-        		$("div.modal").delay(200).hide(100);
-        		$("div.modalContent").hide(200);
-				$("div.modalContent").html('');	
-			}
-		});
-
-		$("div.modalContent").click( function (e) {
-    		// If you put the modal content inside modal background you need to
-    		// stop propagation of the click event so that clicking inside
-    		// the modal content doesn't close the modal
-    
-    		// Prevent click event to bubble up to div.modal
-    		//e.stopPropagation();
-		});
 		
-
 	
 //fin initialize
 }
